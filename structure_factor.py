f4682fc530fdd82ecf73127830c8f2fb3f76bc21
Alexei Lazutin <lazutin@polly.phys.msu.ru>
2023-12-04 17:33:17 +0300

    remove duplicated functions

--- structure_factor.py
+++ structure_factor.py
@@ -289,159 +289,6 @@ def Cartesian_np(rtp):
     ptsnew[:,2] = rtp[:,0] * np.cos(rtp[:,1]) 
     return ptsnew
 
-def structure_factor_cupy(rf,qs,points_num,fmean):
-  
-  s = time.time()
-  r = rf[:,:3]
-  f = rf[:,3] 
-  
-  f = f - fmean
-
-  natom = f.shape[0]
-
-  z2_arr, z2_arr_step = np.linspace(1,-1,num=points_num,endpoint=False,retstep=True)
-  z2_arr = z2_arr + 0.5*z2_arr_step
-
-  sp_dlong = np.pi*(3.0-np.sqrt(5.0))
-
-  phi2_arr, phi2_arr_step = np.linspace(0,points_num*sp_dlong,num=points_num,endpoint=False,retstep=True)
-
-
-  r2_arr = np.sqrt(1.0-z2_arr*z2_arr)
-  rho2_arr = np.sqrt(r2_arr*r2_arr+z2_arr*z2_arr)
-  theta2_arr = np.arctan2(r2_arr,z2_arr)
-
-  pt_pairn = np.empty((phi2_arr.shape[0],3))
-
-  pt_pairn[:,0] = rho2_arr
-  pt_pairn[:,1] = theta2_arr
-  pt_pairn[:,2] = phi2_arr
-
-  phi_theta = pt_pairn
-
-  xyz = Cartesian_np(phi_theta)
-
-  r_mys = np.array_split(qs,qs.shape[0])
-
-  res_qs_arr = []
-  res_abs_arr = []
-  res_sq_arr = []
-  l = len(r_mys)
-  printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
-    
-  for ir_my,r_my in enumerate(r_mys):
-    xyz2 = np.einsum('i,jk',r_my,xyz)
-        
-    xyz2_gpu = cp.asarray(xyz2)
-    r_gpu = cp.asarray(r)
-
-    res_gpu = cp.einsum('ik,zlk',r_gpu,xyz2_gpu)
-    res2_gpu = cp.exp(1j*res_gpu)    
-    
-    f_gpu = cp.asarray(f)
-    res3_gpu = cp.einsum('ikz,i',res2_gpu,f_gpu)
-    res3_gpu = res3_gpu * np.conjugate(res3_gpu)
-    
-    res3 = cp.asnumpy(res3_gpu)
-
-    res3_abs = np.abs(res3)
-    res3_sq = res3.real**2 + res3.imag**2    
-
-    res4_abs = np.sum(res3_abs,axis=0)
-    res4_sq = np.sum(res3_sq,axis=0)
-    
-    res4_abs = res4_abs / (points_num) / natom
-    res4_sq = res4_sq / (points_num) / natom / natom
-
-    res_abs_arr.append(res4_abs)
-    res_sq_arr.append(res4_sq)
-    res_qs_arr.append(r_my)
-    
-   
-    printProgressBar(ir_my + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
-
-  res_abs_out = np.concatenate(res_abs_arr,axis=None)
-  res_sq_out = np.concatenate(res_sq_arr,axis=None)
-  res_qs_out = np.concatenate(res_qs_arr,axis=None)
-
-  return res_abs_out,res_sq_out,res_qs_out
-
-def structure_factor_cupy_directions(rf,qs,points_num,fmean):
-  
-  s = time.time()
-  r = rf[:,:3]
-  f = rf[:,3] 
-  
-  f = f - fmean
-
-  natom = f.shape[0]
-
-  z2_arr, z2_arr_step = np.linspace(1,-1,num=points_num,endpoint=False,retstep=True)
-  z2_arr = z2_arr + 0.5*z2_arr_step
-
-  sp_dlong = np.pi*(3.0-np.sqrt(5.0))
-
-  phi2_arr, phi2_arr_step = np.linspace(0,points_num*sp_dlong,num=points_num,endpoint=False,retstep=True)
-
-  r2_arr = np.sqrt(1.0-z2_arr*z2_arr)
-  rho2_arr = np.sqrt(r2_arr*r2_arr+z2_arr*z2_arr)
-  theta2_arr = np.arctan2(r2_arr,z2_arr)
-
-  pt_pairn = np.empty((phi2_arr.shape[0],3))
-
-  pt_pairn[:,0] = rho2_arr
-  pt_pairn[:,1] = theta2_arr
-  pt_pairn[:,2] = phi2_arr
-
-  phi_theta = pt_pairn
-
-  xyz = Cartesian_np(phi_theta)
-
-  r_mys = np.array_split(qs,qs.shape[0])
-
-  res_qs_arr = []
-  res_abs_arr = []
-  res_sq_arr = []
-  res_xyz_arr = []
-  l = len(r_mys)
-  printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
-    
-  for ir_my,r_my in enumerate(r_mys):
-    xyz2 = np.einsum('i,jk',r_my,xyz)
-        
-    xyz2_gpu = cp.asarray(xyz2)
-    r_gpu = cp.asarray(r)
-
-    res_gpu = cp.einsum('ik,zlk',r_gpu,xyz2_gpu)
-    res2_gpu = cp.exp(1j*res_gpu)
-    
-    f_gpu = cp.asarray(f)
-    res3_gpu = cp.einsum('ikz,i',res2_gpu,f_gpu)
-    
-    res3_gpu = res3_gpu * np.conjugate(res3_gpu)
-    
-    res3 = cp.asnumpy(res3_gpu)
-
-    res3_abs = np.abs(res3)
-    res3_sq = res3.real**2 + res3.imag**2
-
-    res3_abs = res3_abs / natom
-    res3_sq = res3_sq / natom / natom
-
-    res_abs_arr.append(res3_abs)
-    res_sq_arr.append(res3_sq)
-    res_qs_arr.append(r_my)
-    res_xyz_arr.append(xyz2)
-   
-    printProgressBar(ir_my + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
-
-  res_abs_out = np.squeeze(np.stack(res_abs_arr,axis=0))
-  res_sq_out  = np.squeeze(np.stack(res_sq_arr,axis=0))
-  res_qs_out  = np.squeeze(np.stack(res_qs_arr,axis=0))
-  res_xyz_out = np.squeeze(np.stack(res_xyz_arr,axis=0))
-
-  return res_abs_out,res_sq_out,res_qs_out,res_xyz_out,xyz,phi_theta
-
 def structure_factor_cupy_everything(rf,qs,points_num,fmean,directions=False):
   
   s = time.time()
@@ -506,13 +353,13 @@ def structure_factor_cupy_everything(rf,qs,points_num,fmean,directions=False):
         res4_sq = np.mean(res3_sq,axis=0)
     else:
         res4_abs = res3_abs
-        res4_sq  = res4_sq
+        res4_sq  = res3_sq
 
     res4_abs = res4_abs / natom
     res4_sq = res4_sq / natom / natom
 
-    res_abs_arr.append(res3_abs)
-    res_sq_arr.append(res3_sq)
+    res_abs_arr.append(res4_abs)
+    res_sq_arr.append(res4_sq)
     res_qs_arr.append(r_my)
 
     if directions:
@@ -705,25 +552,25 @@ if __name__ == "__main__":
       with cp.cuda.Device(args.gpu):
        if not args.diff:
            if not args.directions:
-               res_abs,res_sq,res_qs = structure_factor_cupy(rf, qs, args.vec, 0.0)
+               res_abs,res_sq,res_qs = structure_factor_cupy_everything(rf, qs, args.vec, 0.0, False)
            else:
-               res_abs,res_sq,res_qs,xyz_all,xyz_sphere,phi_theta = structure_factor_cupy_directions(rf, qs, args.vec, 0.0)
+               res_abs,res_sq,res_qs,xyz_all,xyz_sphere,phi_theta = structure_factor_cupy_everything(rf, qs, args.vec, 0.0, True)
        else:
            if not args.directions:
-               res_abs,res_sq,res_qs = structure_factor_cupy(rf, qs, args.vec, f.mean())
+               res_abs,res_sq,res_qs = structure_factor_cupy_everything(rf, qs, args.vec, f.mean(), False)
            else:
-               res_abs,res_sq,res_qs,xyz_all,xyz_sphere,phi_theta = structure_factor_cupy_directions(rf, qs, args.vec, f.mean())
+               res_abs,res_sq,res_qs,xyz_all,xyz_sphere,phi_theta = structure_factor_cupy_everything(rf, qs, args.vec, f.mean(),True)
      else:
        if not args.diff:
            if not args.directions:
-               res_abs,res_sq,res_qs = structure_factor_cupy(rf, qs, args.vec, 0.0)
+               res_abs,res_sq,res_qs = structure_factor_cupy_everything(rf, qs, args.vec, 0.0, False)
            else:
-               res_abs,res_sq,res_qs,xyz_all,xyz_sphere,phi_theta = structure_factor_cupy_directions(rf, qs, args.vec, 0.0)
+               res_abs,res_sq,res_qs,xyz_all,xyz_sphere,phi_theta = structure_factor_cupy_everything(rf, qs, args.vec, 0.0, True)
        else:
            if not args.directions:
-               res_abs,res_sq,res_qs = structure_factor_cupy(rf, qs, args.vec, f.mean())
+               res_abs,res_sq,res_qs = structure_factor_everything(rf, qs, args.vec, f.mean(), False)
            else:
-               res_abs,res_sq,res_qs,xyz_all,xyz_sphere,phi_theta = structure_factor_cupy_directions(rf, qs, args.vec, f.mean())
+               res_abs,res_sq,res_qs,xyz_all,xyz_sphere,phi_theta = structure_factor_cupy_everything(rf, qs, args.vec, f.mean(), True)
     
      e = time.time()
      print('{:.1f} s used '.format(e-s), end='')
